932eabdda4497554b681bc9ba3cfe9f8840261c7
adamnovotny1 <vasemailnagithub@gmail.com>
2025-02-03 14:23:11 +0100

    Základní připojení css

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return ("Hello World!<br>"
-            "<a href='/kontakt'>Kontakt</a>"
-            )
+    return render_template('index.html')
 @app.route('/kontakt')
 def contact():
     return "Kontakt"
 
-@app.route('/<name>')
+@app.route('/jmeno/<name>')
 def hello(name):
     return f"<h1>Hello  {name}</h1>"
+
+@app.route('/<a>/<b>')
+def multiply(a,b):
+    try:
+        multiplier = int(a) * int(b)
+        return f"Násobek je {multiplier}"
+    except ValueError:
+        return ("Neplatné číslo")
 
 if __name__ == '__main__':
     app.run(debug=True)
