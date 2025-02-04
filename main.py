c5651a6247fc1065434f678b0951bd41768423db
adamnovotny1 <vasemailnagithub@gmail.com>
2025-02-04 15:41:07 +0100

    Základní CSS

--- main.py
+++ main.py
@@ -2,24 +2,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
 @app.route('/kontakt')
 def contact():
-    return "Kontakt"
+    return render_template('contact.html')
 
-@app.route('/jmeno/<name>')
-def hello(name):
-    return f"<h1>Hello  {name}</h1>"
 
-@app.route('/<a>/<b>')
-def multiply(a,b):
-    try:
-        multiplier = int(a) * int(b)
-        return f"Násobek je {multiplier}"
-    except ValueError:
-        return ("Neplatné číslo")
+@app.route('/uzivatel/<username>')
+def user(username):
+    return render_template('user.html', username=username)
+
+
+# @app.route('/<a>/<b>')
+# def multiply(a,b):
+#     try:
+#         multiplier = int(a) * int(b)
+#         return f"Násobek je {multiplier}"
+#     except ValueError:
+#         return ("Neplatné číslo")
 
 if __name__ == '__main__':
     app.run(debug=True)
