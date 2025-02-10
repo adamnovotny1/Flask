c8c0a6f2b09ef3e53991f3bf04b6540b08c2c502
adamnovotny1 <vasemailnagithub@gmail.com>
2025-02-10 10:12:08 +0100

    Implementace šablony base.html

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
 
 @app.route('/kontakt')
 def contact():
