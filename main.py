539d4bdc65ff6f1a6fbc535b513ee7916a21391c
adamnovotny1 <vasemailnagithub@gmail.com>
2025-02-24 09:24:35 +0100

    Flash Massages

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
-
+app.config["SECRET_KEY"] = "dev"
 
 @app.route('/')
 def index():
@@ -27,8 +27,23 @@ def link():
         username = request.form['username']
         password = request.form['password']
         radio = request.form['radio']
+        flash("Login Succesful")
+        flash("Login Unsuccesful","warning")
         return render_template('zkouska.html', username=username, password=password, radio=radio)
+
     return render_template('link.html')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username == 'admin' and password == 'heslo':
+            flash("Login Succesful")
+            return render_template('login.html', login = True)
+        else:
+            flash("Login Unsuccesful", "warning")
+            return render_template('login.html', login = False)
+    return render_template('login.html')
 
 # @app.route('/<a>/<b>')
 # def multiply(a,b):
